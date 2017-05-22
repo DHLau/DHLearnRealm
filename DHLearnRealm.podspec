50265ae51bec0bf307e4dff3f6429ff9b95ab902
liudunhui <liudunhui@11wlw.com>
2017-05-22 16:17:08 +0800

    修改文件

--- DHLearnRealm.podspec
+++ DHLearnRealm.podspec
@@ -40,5 +40,5 @@ TODO: Add long description of the pod here.
   # s.frameworks = 'UIKit', 'MapKit'
   # s.dependency 'AFNetworking', '~> 2.3'
   # please Open Realm 
-  # s.dependency 'Realm'
+  s.dependency 'Realm'
 end
--- Example/Pods/Target Support Files/Pods-DHLearnRealm_Example/Pods-DHLearnRealm_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-DHLearnRealm_Example/Pods-DHLearnRealm_Example-frameworks.sh
@@ -90,9 +90,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/DHLearnRealm/DHLearnRealm.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Realm/Realm.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/DHLearnRealm/DHLearnRealm.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Realm/Realm.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
